08952b2e695a335458875d87ca5a6bec38d8a715
Samuel Leite <samuelleite.fl@gmail.com>
2024-06-09 20:30:56 -0400

    database

--- app.js
+++ app.js
@@ -1,22 +1,35 @@
+//express
+const client = require('./databasepg.js')
 const express = require("express")
 const app = express();
 const port = 3000;
 
+// handlebars
 const exphbs = require('express-handlebars')
 const hbs = exphbs.create({});
+
 
 app.engine('handlebars', hbs.engine);
 
 app.set('view engine', 'handlebars');
 app.use(express.static('public'))
 
-app.get("/", (req, res) => {
+app.get("/passwords", (req, res) => {
     res.render('main', {layout: 'index'});
+    client.query(`Select * from passwords`, (err, result)=>{
+        if(!err){
+            res.send(result.rows);
+        }
+    });
+    client.end;
 });
 
 app.listen(port, () => {
     console.log(`App Listening to port ${port}`)
 });   
+
+client.connect();
+
 
 const { Sequelize } = require('sequelize');
 
